af613998d9dd024c764cdadc66925a692f0878a4
ziadAlattar <ziadalattar22@gmail.com>
2024-08-12 16:41:43 +0300

    'testApi'

--- src/app/services/blog.service.ts
+++ src/app/services/blog.service.ts
@@ -5,7 +5,7 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class BlogService {
-  private baseUrl = 'bdc-apis-ziadal3tars-projects.vercel.app/blog';
+  private baseUrl = 'http://localhost:3000/blog';
 
   constructor(private http: HttpClient) {}
   addBlog(data: any): any {
--- src/app/services/category.service.ts
+++ src/app/services/category.service.ts
@@ -5,7 +5,7 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class CategoryService {
-  private baseUrl = 'bdc-apis-ziadal3tars-projects.vercel.app/category';
+  private baseUrl = 'http://localhost:3000/category';
 
   constructor(private http: HttpClient) {}
   addCategory(data: any): any {
